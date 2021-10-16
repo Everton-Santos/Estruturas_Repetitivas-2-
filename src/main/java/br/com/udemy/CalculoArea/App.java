8d9ccde7dbe3fb35741e19ad89d46a994095752e
Everton Santos <everton_santos_01@hotmail.com>
2021-10-15 23:14:21 -0300

    try_catch

--- src/main/java/br/com/udemy/CalculoArea/App.java
+++ src/main/java/br/com/udemy/CalculoArea/App.java
@@ -1,5 +1,6 @@
 package br.com.udemy.CalculoArea;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,17 +11,32 @@ public class App {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Largura: ");
-		double largura = sc.nextDouble();
+		try {
+			System.out.print("Largura: ");
+			double largura = sc.nextDouble();
+			
+			System.out.print("Comprimento: ");
+			double comprimento = sc.nextDouble();
+			
+			System.out.print("Metro Quadrado: ");
+			double metroQuadrado = sc.nextDouble();
+			
+			CalculoArea ca = new CalculoArea();
+			System.out.printf("\nArea = %.2f metros\n", ca.calculaArea(largura, comprimento));
+			System.out.printf("Preço = %.2f$\n", ca.precoArea(metroQuadrado));
+			
+			System.out.println("\nPrograma encerrado com sucesso!");
+			
+		} catch (NullPointerException n) {
+			System.err.println("\n*** O campo não pode ser nulo ***");
+		} catch (InputMismatchException e) {
+			System.err.println("\n*** Caracter inválido ***");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
 		
-		System.out.print("Comprimento: ");
-		double comprimento = sc.nextDouble();
 		
-		System.out.print("Metro Quadrado: ");
-		double metroQuadrado = sc.nextDouble();
-		
-		CalculoArea ca = new CalculoArea();
-		System.out.printf("Area = %.2f metros\n", ca.calculaArea(largura, comprimento));
-		System.out.printf("Preço = %.2f$\n", ca.precoArea(metroQuadrado));
 	}
 }
